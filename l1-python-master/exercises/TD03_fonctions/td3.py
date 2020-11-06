4c791b2420483ff0e0a6983a38c84ed11b176a82
Paula POP <paula.pop@ens.uvsq.fr>
2020-11-06 09:53:00 +0100

    td3 partie affiche deboguée

--- l1-python-master/exercises/TD03_fonctions/td3.py
+++ l1-python-master/exercises/TD03_fonctions/td3.py
@@ -1,10 +1,11 @@
-#temps[0] : jours, temps[1]: heures, temps[2]: minutes, temps[3]: secondes
+# temps[0] : jours, temps[1]: heures, temps[2]: minutes, temps[3]: secondes
 
 def tempsEnSeconde(temps):
     """ Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde."""
     return (temps[0]*24*3600+temps[1]*3600+temps[2]*60+temps[3])
 
-temps = (3,23,1,34)
+
+temps = (3, 23, 1, 34)
 print(type(temps))
 print(tempsEnSeconde(temps))   
 
@@ -18,3 +19,29 @@ def secondeEnTemps(seconde):
     
 temps = secondeEnTemps(100000)
 print(temps[0],"jours",temps[1],"heures",temps[2],"minutes",temps[3],"secondes")
+
+#fonction auxiliaire ici
+
+def afficheTemps(temps):
+
+    """Affiche le temps"""
+    if temps[0]==1:
+        print(temps[0], "jour", end=" ")
+    elif temps[0]>1:
+        print(temps[0], "jours", end=" ")
+    if temps[1]==1:
+        print(temps[1], "heure", end=" ")
+    elif temps[1]>1:
+        print(temps[1], "heures", end=" ")
+    if temps[2]==1:
+        print(temps[2], "minute", end=" ")
+    elif temps[2]>1:
+        print(temps[2], "minutes", end=" ")
+    if temps[3]==1:
+        print(temps[3], "seconde", end=" ")
+    elif temps[3]>1:
+        print(temps[3], "secondes", end=" ")
+    return temps
+
+afficheTemps((1,0,14,23))
+
